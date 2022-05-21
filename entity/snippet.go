c525eaafb08a36e4d0f6fd5723bd031dc86f453a
Rupin Jairaj <rupinjairaj@gmail.com>
2022-05-21 01:04:22 -0500

    add snippet 'name' field

--- entity/snippet.go
+++ entity/snippet.go
@@ -2,11 +2,13 @@ package entity
 
 type SnippetFirestore struct {
 	Id      int64    `json:"id"`
+	Name    string   `json:"name"`
 	TagIds  []string `json:"tagIds"`
 	Content string   `json:"content"`
 }
 
 type SnippetClient struct {
+	Name    string   `json:"name"`
 	Tags    []string `json:"tags"`
 	Content string   `json:"content"`
 }
